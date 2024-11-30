b10c490dca716e86db0e771ae362cb4279a96fb6
Max Rozdobudko <max.rozdobudko@gmail.com>
2024-11-30 11:36:11 +0200

    doc: simplifies demo app code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,14 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Crop Image Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Crop Image Widget'),
+      home: MyHomePage(title: 'Crop Image Widget'),
     );
   }
 }
@@ -38,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   late final _controller = CropController();
 
-  // MARK: - Lifecycle
+  // MARK: Lifecycle
 
   @override
   Widget build(BuildContext context) {
@@ -62,54 +58,39 @@ class _MyHomePageState extends State<MyHomePage> {
               });
             },
             icon: const Icon(Icons.crop),
-          )
+          ),
         ],
       ),
       backgroundColor: theme.colorScheme.surfaceContainerHigh,
       body: Padding(
         padding: const EdgeInsets.all(32.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: CropImage(
-                image: const AssetImage('assets/bowl.jpg'),
-                controller: _controller,
-                cropArea: CropArea.aspectRatio(16 / 9,
-                  isEditable: true,
-                  margin: 32,
-                ),
-                settings: const CropSettings(
-                  constraints: ConstraintsSettings.viewportConstrained(),
-                  zoom: ZoomSettings.defaults(),
-                ),
-              ),
-            ),
-          ]
+        child: CropImage(
+          image: const AssetImage('assets/bowl.jpg'),
+          controller: _controller,
+          cropArea: CropArea.aspectRatio(16 / 9,
+            isEditable: true,
+            margin: 32,
+          ),
+          settings: const CropSettings(
+            constraints: ConstraintsSettings.viewportConstrained(),
+            zoom: ZoomSettings.defaults(),
+          ),
         ),
       ),
     );
   }
 
-  // MARK: - Handlers
+  // MARK: Handlers
 
   Future _showCroppedImageDialog(
     Uint8List bytes,
     ImageByteFormat format,
   ) async {
-    showDialog(
+    await showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text('Complete'),
-          content: Text('Bytes length of cropped image : ${bytes.length}'),
-          actions: [
-            TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('OK')
-            )
-          ],
+          content: Image.memory(bytes),
         );
       }
     );
